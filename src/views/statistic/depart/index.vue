869aedfab11a05513a10ef359b2ada87e9bd97d0
陈鸿鹏 <chenhongpeng@xiamenair.com>
2020-01-06 14:56:41 +0800

    调样式、我疯了

--- src/views/statistic/depart/index.vue
+++ src/views/statistic/depart/index.vue
@@ -54,10 +54,12 @@
               </el-col>
             </el-row>
           </div>
-          <div>
+          <div class="chart-pannel">
             <div class="chart-group">
               <echart class="first-pie-chart" ref="firstPieDom" auto-resize :options="firstPieOption"></echart>
-              <echart class="first-line-chart" ref="firstLineDom" auto-resize :options="firstLineOption"></echart>
+              <div style="height: 100px;">
+                <echart class="first-line-chart" ref="firstLineDom" auto-resize :options="firstLineOption"></echart>
+              </div>
             </div>
           </div>
         </el-card>
@@ -171,6 +173,7 @@ export default {
             type: "pie",
             radius: "40%",
             center: ["40%", "30%"],
+            color: ['#00D6F2', '#36F0F2', '#1EA8D1', '#0482B3', '#1C6C80'],
             data: this.list.map(item => {
               return {
                 name: item.departName,
@@ -189,67 +192,58 @@ export default {
       };
     },
     initFirstLineChart() {
+      if (!this.lineData.length) {
+        return
+      }
+      let legendData = this.lineData.map(item => item[0].departName)
+      let xAxisData = this.lineData[0].map(item => item.date)
+      let seriesData = this.lineData.map((item, index) => {
+        return {
+          name: item[0].departName,
+          type: 'line',
+          stack: '加班时长',
+          data: item.map(inner => +inner.allOverTime),
+          color: ['#00D6F2', '#36F0F2', '#1EA8D1', '#0482B3', '#1C6C80'][index]
+        }
+      })
+
       this.firstLineOption = {
         title: {
-        text: '折线图堆叠'
-    },
-    tooltip: {
-        trigger: 'axis'
-    },
-    legend: {
-        data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
-    },
-    grid: {
-        left: '3%',
-        right: '10%',
-        bottom: '3%',
-        containLabel: true
-    },
-    toolbox: {
-        feature: {
-            saveAsImage: {}
-        }
-    },
-    xAxis: {
-        type: 'category',
-        boundaryGap: false,
-        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
-    },
-    yAxis: {
-        type: 'value'
-    },
-    series: [
-        {
-            name: '邮件营销',
-            type: 'line',
-            stack: '总量',
-            data: [120, 132, 101, 134, 90, 230, 210]
+          text: '部门加班曲线图',
+          textStyle: {
+            fontSize: 14
+          },
+          subText: "纯属虚构"
         },
-        {
-            name: '联盟广告',
-            type: 'line',
-            stack: '总量',
-            data: [220, 182, 191, 234, 290, 330, 310]
+        tooltip: {
+          trigger: 'axis'
         },
-        {
-            name: '视频广告',
-            type: 'line',
-            stack: '总量',
-            data: [150, 232, 201, 154, 190, 330, 410]
+        legend: {
+          left: 'right',
+          top: 'center',
+          orient: 'vertical',
+          data: legendData
         },
-        {
-            name: '直接访问',
-            type: 'line',
-            stack: '总量',
-            data: [320, 332, 301, 334, 390, 330, 320]
+        grid: {
+          left: '3%',
+          right: '10%',
+          top: 'top',
+          containLabel: true
         },
-        {
-            name: '搜索引擎',
-            type: 'line',
-            stack: '总量',
-            data: [820, 932, 901, 934, 1290, 1330, 1320]
-        }
-    ]
+        toolbox: {
+            feature: {
+              saveAsImage: {}
+            }
+        },
+        xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: xAxisData
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: seriesData
       }
     },
     getAllPart () {
@@ -280,6 +274,7 @@ export default {
       } else {
         departNos = this.selectedOrg.map(item => item.departNo)
       }
+      console.log(startTime, endTime)
       return new Promise((resolve, reject) => {
         getDepartLine({
           startTime,
@@ -293,16 +288,119 @@ export default {
         })
       })
     },
+    getValue (str) {
+      let count = 0
+      for (let i = 0; i < str.length; i++) {
+        count += str[i].charCodeAt()
+      }
+      return count
+    },
     handleLineData (arr) {
-      arr = arr || []
+      const me = this
+      arr = arr.sort((a, b) => {
+        return me.getValue(a.departNo) - me.getValue(b.departNo)
+      })
       let obj = {}
-      let test = []
+      let res = []
+      // xAxis: ['2019-01-01', '2019-01-02']
+      // data: [{name: '电商', data: [1, 2]}]
+      let tem = []
       for (let i = 0; i < arr.length; i++) {
-        if (arr[i].departNo === 'T001') {
-          test.push(arr[i].date)
+        if (!obj[arr[i].departName]) {
+          tem = []
+          obj[arr[i].departName] = true
+          res.push(tem)
+        } else {
+          tem.push(arr[i])
         }
       }
-      console.log(test)
+      // 造数据
+      const oneDay = 1000 * 60 * 60 * 24
+      const nowDate = +new Date()
+      const endTime = dayToString(nowDate)
+
+      let startTime
+      if (this.commonTime === '近一周') {
+        for (let j = 0; j < res.length; j++) {
+          startTime = dayToString(nowDate - 7 * oneDay)
+          // 用来复制数据
+          const copyItem = res[j] && res[j][0] || {}
+          // 用于判断数据中是否有这个日期
+          let dateArr = res[j].map(item => item.date)
+          console.log('dateARr', dateArr)
+          for (let k = 0; k < 7; k++) {
+            if (!dateArr.includes(startTime)) {
+              res[j].push({
+                departNo: copyItem.departNo,
+                departName: copyItem.departName,
+                allOverTime: 0,
+                date: startTime,
+                cost: 0
+              })
+            }
+            startTime = dayToString(+new Date(+new Date(startTime) + oneDay))
+          }
+          res[j] = res[j].sort((a, b) => {
+            const aValue = +new Date(a.date)
+            const bValue = +new Date(b.date)
+            return aValue - bValue
+          })
+        }
+      } else if (this.commonTime === '近一月') {
+        for (let j = 0; j < res.length; j++) {
+          startTime = dayToString(nowDate - 30 * oneDay)
+          // 用来复制数据
+          const copyItem = res[j] && res[j][0] || {}
+          // 用于判断数据中是否有这个日期
+          let dateArr = res[j].map(item => item.date)
+          console.log('dateARr', dateArr)
+          for (let k = 0; k < 30; k++) {
+            if (!dateArr.includes(startTime)) {
+              res[j].push({
+                departNo: copyItem.departNo,
+                departName: copyItem.departName,
+                allOverTime: 0,
+                date: startTime,
+                cost: 0
+              })
+            }
+            startTime = dayToString(+new Date(+new Date(startTime) + oneDay))
+          }
+          res[j] = res[j].sort((a, b) => {
+            const aValue = +new Date(a.date)
+            const bValue = +new Date(b.date)
+            return aValue - bValue
+          })
+        }
+      } else {
+        for (let j = 0; j < res.length; j++) {
+          startTime = dayToString(nowDate - 365 * oneDay)
+          // 用来复制数据
+          const copyItem = res[j] && res[j][0] || {}
+          // 用于判断数据中是否有这个日期
+          let dateArr = res[j].map(item => item.date)
+          console.log('dateARr', dateArr)
+          for (let k = 0; k < 365; k++) {
+            if (!dateArr.includes(startTime)) {
+              res[j].push({
+                departNo: copyItem.departNo,
+                departName: copyItem.departName,
+                allOverTime: 0,
+                date: startTime,
+                cost: 0
+              })
+            }
+            startTime = dayToString(+new Date(+new Date(startTime) + oneDay))
+          }
+          res[j] = res[j].sort((a, b) => {
+            const aValue = +new Date(a.date)
+            const bValue = +new Date(b.date)
+            return aValue - bValue
+          })
+        }
+      }
+      
+      return res
     },
     getDepartOvertime () {
       // 一天的值
@@ -355,7 +453,7 @@ export default {
 };
 </script>
 
-<style lang='less' scoped>
+<style lang='scss'>
 .all-height {
   height: 100%;
 }
@@ -373,18 +471,37 @@ export default {
   }
   .chart-wrap {
     flex: 1;
-    .chart-group {
-      width: 100%;
-      display: flex;
-      .first-pie-chart {
-        // flex: 1;
-        max-width: 400px;
-      }
-      .first-line-chart {
-        flex: 1;
-        // max-width: 400px;
-      }
+    .chart-panel {
+      display: block;
+      height: 100%;
+    };
+
+    // &.el-card > .el-card__body {
+    //   height: calc(100% - 100px) !important;
+    //   > .chart-pannel {
+    //     height: 100%;
+    //     > .chart-group {
+    //       height: 49%;
+    //       margin-bottom: 0.5%;
+    //       > .first-line-chart,
+    //       > .first-pie-chart {
+    //         width: 50%;
+    //         height: 100%;
+    //       }
+    //     }
+    //   }
+    // }
+    .first-pie-chart {
+      width: 500px;
+      height: 320px;
     }
+  }
+
+  .four-chart {
+    display: inline-block;
+    width: 48%;
+    height: 100%;
+    // background: #eee;
   }
 
   .rank-header {
@@ -403,8 +520,9 @@ export default {
     flex-flow: column;
     height: 500px;
     background: #667db6;  /* fallback for old browsers */
-    background: -webkit-linear-gradient(to right, #667db6, #0082c8, #0082c8, #667db6);  /* Chrome 10-25, Safari 5.1-6 */
-    background: linear-gradient(to right, #667db6, #0082c8, #0082c8, #667db6); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    // background: -webkit-linear-gradient(to right, #DABEE8, #0082c8, #0082c8, #00D6F2);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #D9FFA3, #B0CCFF); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    background: linear-gradient(-45deg, #00D6F2 100px, #0482B3 200px);
 
     padding: 10px;
     border-radius: 4px;
@@ -459,7 +577,7 @@ export default {
   }
 
   .tag-wrap {
-    margin-top: 5px;
+    margin-top: 4px;
     float: right;
     font-size: 12px;
     height: 26px;
